9f5896e3e4f4e7fa93956e22c927e193a15ef03c
Norhan-Khaled <107856437+Norhan-Khaled@users.noreply.github.com>
2022-07-09 02:49:33 +0300

    not sure of the bonus point

--- script.js
+++ script.js
@@ -6,8 +6,37 @@ let A_grades_div = document.getElementById("A-grades");
 let bonus_grades_div = document.getElementById("bonus-grades");
 let random_grade_div = document.getElementById("random-grade");
 
-
 function search() {
   let inputValue = document.getElementById("input").value;
-  
+  let marks = grades.filter((marks) => marks == inputValue);
+  search_grades_div.innerHTML += `<span> ${marks} </span>`;
 }
+
+grades.pop();
+grades.push(99);
+
+// grades.forEach(
+//   (grade) => (all_grades_div.innerHTML += `<span>${grade}</span>`)
+// );
+
+for (let i = 0; i < grades.length; i++) {
+  all_grades_div.innerHTML += `<span> ${grades[i]} </span>`;
+}
+
+let excellent = grades.filter((excellent) => excellent > 90);
+
+A_grades_div.innerHTML += `<span> ${excellent} </span>`;
+
+let addition = grades.map((addition) => addition + 3);
+bonus_grades_div.innerHTML += `<span> ${addition} </span>`;
+
+// if ((inputValue = document.getElementById("input").value) != grades) {
+//   alert("not found");
+// }
+
+var randomIndex = Math.floor(Math.random() * grades.length);
+
+random_grade_div.innerHTML += `<span>
+  
+    #${randomIndex}: ${grades[randomIndex]}
+  </span>`;
